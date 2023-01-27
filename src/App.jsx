e39a1fcc3c680e8aeb50624adfdb46825e841802
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-01-27 10:09:50 -0300

    Footer component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import PostList from './components/PostList'
 import GlobalStyle from './styles/GlobalStyle'
@@ -9,6 +10,7 @@ function App() {
       <GlobalStyle />
       <Header />
       <PostList />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from './styles'
+
+function Footer() {
+  return (
+    <Container>
+      <span>JStack's Blog. Todos os direitos reservados.</span>
+      <button type="button">🌞</button>
+    </Container>
+  )
+}
+
+export default Footer
