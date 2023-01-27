079fb601b75ea3437e4aa041515f2d99efd4007d
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-01-27 09:42:12 -0300

    Header component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from './components/Header'
 import GlobalStyle from './styles/GlobalStyle'
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Header/>
     </>
   )
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from './styles'
+
+function Header() {
+  return (
+    <Container>
+      <h1>JStack's Blog</h1>
+      <button type="button">🌞</button>
+    </Container>
+  )
+}
+
+export default Header
